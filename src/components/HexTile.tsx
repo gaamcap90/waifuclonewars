512d212a78758d06f1a4a6a58485179b9ff648bb
gaamcap90 <gaamcap14@hotmail.com>
2025-08-04 17:37:47 +0900

    Update HexTile.tsx

--- src/components/HexTile.tsx
+++ src/components/HexTile.tsx
@@ -35,7 +35,7 @@ export default function HexTile({
 }: HexTileProps) {
   // 1) Terrain → public URL
   const terrainMap: Record<string,string> = {
-    forest:       "/uploads/Forest.png",
+    forest:       "/uploads/Forest_2.png",
     mountain:     "/uploads/Mountains_2.png",
     river:        "/uploads/River.png",
     plain:        "/uploads/Plain.png",
